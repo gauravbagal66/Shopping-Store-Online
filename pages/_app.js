64d14148ce78f335d8c790c9b1b7f25d724aff3d
gauravbagal66 <106031353+gauravbagal66@users.noreply.github.com>
2025-05-07 22:16:35 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
+
 config.autoAddCss = false;
 
 export default MyApp;
